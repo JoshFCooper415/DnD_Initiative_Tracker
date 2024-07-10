f5f4933e5ac3ee1a16235ce0a57e2dd3b461cb50
JoshFCooper415 <joshfcooper@vt.edu>
2024-07-09 22:46:15 -0400

    almost done

--- backend.py
+++ backend.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 CORS(app)
 
 initiatives = []
-color_order = []
-current_turn_index = 0
 connected_devices = {}
 host_device_id = None
+current_turn_index = 0
 
 DISTINCT_COLORS = [
     "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF",
@@ -37,7 +36,6 @@ def connect_device():
     connected_devices[device_id] = {"color": color, "isHost": is_host}
     return jsonify({"device_id": device_id, "color": color, "isHost": is_host})
 
-
 @app.route('/api/disconnect/<string:device_id>', methods=['POST'])
 def disconnect_device(device_id):
     global host_device_id
@@ -64,34 +62,11 @@ def add_initiative():
         "id": str(uuid.uuid4()),
         "name": data['name'],
         "initiative": int(data['initiative']),
-        "color": data['color'],
-        "isNPC": data.get('isNPC', False)
+        "color": data['color']
     }
     initiatives.append(new_initiative)
     initiatives.sort(key=lambda x: x['initiative'], reverse=True)
     return jsonify(new_initiative), 201
-
-@app.route('/api/npcs', methods=['POST'])
-def add_npc():
-    data = request.json
-    if not data or 'name' not in data or 'initiative' not in data:
-        return jsonify({"error": "Invalid data"}), 400
-    
-    if host_device_id is None or host_device_id not in connected_devices:
-        return jsonify({"error": "Host device not found"}), 400
-    
-    host_color = connected_devices[host_device_id]["color"]
-    
-    new_npc = {
-        "id": str(uuid.uuid4()),
-        "name": data['name'],
-        "initiative": int(data['initiative']),
-        "color": host_color,
-        "isNPC": True
-    }
-    initiatives.append(new_npc)
-    initiatives.sort(key=lambda x: x['initiative'], reverse=True)
-    return jsonify(new_npc), 201
 
 @app.route('/api/initiatives/<string:initiative_id>', methods=['DELETE'])
 def delete_initiative(initiative_id):
@@ -103,32 +78,32 @@ def delete_initiative(initiative_id):
 def get_connected_devices():
     return jsonify(connected_devices)
 
-@app.route('/api/current-turn', methods=['GET', 'POST'])
-def current_turn():
+@app.route('/api/next-turn', methods=['POST'])
+def next_turn():
     global current_turn_index
-    if request.method == 'POST':
-        data = request.json
-        if 'id' in data:
-            try:
-                current_turn_index = next(i for i, init in enumerate(initiatives) if init['id'] == data['id'])
-                current_turn_index = (current_turn_index + 1) % len(initiatives)
-            except StopIteration:
-                return jsonify({"error": "Invalid initiative ID"}), 400
-            return jsonify({"message": "Turn updated"}), 200
-        return jsonify({"error": "Invalid data"}), 400
-    else:
-        if initiatives:
-            current_initiative = initiatives[current_turn_index]
-            return jsonify({
-                "id": current_initiative['id'],
-                "color": current_initiative['color'],
-                "name": current_initiative['name']
-            })
+    if not initiatives:
         return jsonify({"error": "No initiatives"}), 404
+    
+    current_turn_index = (current_turn_index + 1) % len(initiatives)
+    current_initiative = initiatives[current_turn_index]
+    
+    return jsonify({
+        "id": current_initiative['id'],
+        "color": current_initiative['color'],
+        "name": current_initiative['name']
+    }), 200
 
-@app.route('/api/color-order', methods=['GET'])
-def get_color_order():
-    return jsonify(color_order)
+@app.route('/api/current-turn', methods=['GET'])
+def get_current_turn():
+    if not initiatives:
+        return jsonify({"error": "No initiatives"}), 404
+    
+    current_initiative = initiatives[current_turn_index]
+    return jsonify({
+        "id": current_initiative['id'],
+        "color": current_initiative['color'],
+        "name": current_initiative['name']
+    })
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',5000, debug=True)
+    app.run('0.0.0.0',debug=True)
